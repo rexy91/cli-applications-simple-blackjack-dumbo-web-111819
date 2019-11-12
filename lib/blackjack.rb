cd5765032c27886242da6030794a40c85fb7155e
Yu Hao Ye <rxye1991@gmail.com>
2019-11-12 02:01:22 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,53 +1,64 @@
-def deal_card
-  # code #deal_card here
-    rand(1..11)
-end
-
-def display_card_total(card_total)
-  # code #display_card_total here
-  puts "Your cards add up to #{card_total}"
-  card_total
-end
-
 def welcome
   # code #welcome here
   puts "Welcome to the Blackjack Table"
 end
 
+def deal_card
+  # code #deal_card here
+  ran = rand(1..11)
+end
+
+def display_card_total(cardtotal)
+  # code #display_card_total here
+   puts "Your cards add up to #{cardtotal}"
+end
+
 def prompt_user
   # code #prompt_user here
   puts "Type 'h' to hit or 's' to stay"
-end
-
-def initial_round
-  # code #initial_round here
-  sum = rand(1..11) + rand(1..11)
-  display_card_total(sum)
-end
-
-def end_game(card_total)
-  # code #end_game here
-    puts "Sorry, you hit #{card_total}. Thanks for playing!"
+  
 end
 
 def get_user_input
   # code #get_user_input here
-  gets.strip.chomp
+  return gets.chomp
 end
 
-def hit? (card_total)
-  # code hit? here
-  prompt_user
-  if get_user_input == 'h'
-    card_total += deal_card
-  end
-    card_total
+def end_game(cardtotal)
+  # code #end_game here
+  puts "Sorry, you hit 27. Thanks for playing!"
 end
 
 def invalid_command
   # code invalid_command here
-  puts "invalid"
+  "Please enter a valid command"
 end
+
+def initial_round
+  # code #initial_round here
+  sum = 0
+  sum = deal_card + deal_card 
+  return sum 
+  display_card_total(sum)
+end
+
+def hit?(cardtotal)
+  # code hit? here
+  prompt_user
+  answer = get_user_input
+  if answer == 'h'
+      cardtotal = cardtotal + deal_card
+  else if answer == 's'
+      return cardtotal
+  else
+      invalid_command
+      prompt_user
+  end
+  return cardtotal
+  
+end
+end
+
 
 #####################################################
 # get every test to pass before coding runner below #
@@ -55,13 +66,8 @@ end
 
 def runner
   # code runner here
-  welcome  
-  card_total = initial_round
-  while card_total < 21
-    card_total = hit?(card_total)
-    card_total = display_card_total(card_total)
-  end
-  end_game(card_total)
-
+  
 end
     
+
+
